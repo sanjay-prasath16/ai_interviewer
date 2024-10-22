bd1f660a41ff6f36adbef0345b91141f41de0b91
sanjayprasath.kriyaetive@gmail.com <sanjayprasath165@gmail.com>
2024-10-22 22:34:55 +0530

    changed ui spaces on job description

--- Client/src/Pages/applicantsPool.jsx
+++ Client/src/Pages/applicantsPool.jsx
@@ -7,9 +7,10 @@ const applicantsPool = () => {
             <div className="border border-white bg-white w-2/3 h-full ml-10 my-12 rounded-2xl"></div>
             <div className="w-[26%]">
                 <div className="border border-white bg-white h-[45%] ml-4 mt-12 w-full rounded-2xl"></div>
-                <div className="border border-white bg-white h-[45%] ml-4 mt-3 w-full rounded-2xl"></div>
+                <div className="border border-white bg-white h-[45%] ml-4 mt-4 w-full rounded-2xl"></div>
             </div>
         </div>
+        {/* candidate list */}
     </div>
   )
 }
